ffa573d31ed4f0e7328f3fefb5394d0d05393082
sasham43 <sashasemail@gmail.com>
2018-12-02 16:55:16 -0600

    add font back in

--- vs.py
+++ vs.py
@@ -19,9 +19,9 @@ try:
     # bmp = Image.open('/home/pi/14.bmp')
     # Himage2.paste(bmp, (50,10))
     # epd.display(epd.getbuffer(Himage2))
-    # font24 = ImageFont.truetype('/usr/share/fonts/truetype/lato/Lato-Regular.ttf', 24)
+    font24 = ImageFont.truetype('/usr/share/fonts/truetype/lato/Lato-Regular.ttf', 24)
     draw = ImageDraw.Draw(Himage)
-    draw.text((10, 0), 'this si some text, let us see how it goes.  we go we go we go', fill = 0)
+    draw.text((10, 0), 'this si some text, let us see how it goes.  we go we go we go', font = font24, fill = 0)
     epd.display(epd.getbuffer(Himage))
 
     print('sleeping')
