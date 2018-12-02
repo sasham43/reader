a2cd2a79490464c19065c779609787bf8af1e0c6
sasham43 <sashasemail@gmail.com>
2018-12-02 16:33:05 -0600

    rename epd

--- vs.py
+++ vs.py
@@ -4,14 +4,14 @@ from PIL import Image,ImageDraw,ImageFont
 import traceback
 
 try:
-    epd = epd7in5.EPD()
+    epd = epd.EPD()
     epd.init()
     print("Clear")
     epd.Clear(0xFF)
 
     print("Drawing")
     print("read bmp file on window")
-    Himage2 = Image.new('1', (epd7in5.EPD_HEIGHT, epd7in5.EPD_WIDTH), 255)  # 255: clear the frame
+    Himage2 = Image.new('1', (epd.EPD_HEIGHT, epd.EPD_WIDTH), 255)  # 255: clear the frame
     bmp = Image.open('/home/pi/14.bmp')
     Himage2.paste(bmp, (50,10))
     epd.display(epd.getbuffer(Himage2))
