6e5fd08696cb84e8065cd667c7fd67b0a359f82a
sasham43 <sashasemail@gmail.com>
2018-12-02 16:38:38 -0600

    add ursual quote

--- vs.py
+++ vs.py
@@ -3,6 +3,8 @@ import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
 
+ursula = 'It exists… It’s real. I can call it a misunderstanding, but I can’t pretend that it doesn’t exist, or will ever cease to exist. Suffering is the condition on which we live. And when it comes, you know it. You know it as the truth. Of course it’s right to cure diseases, to prevent hunger and injustice, as the social organism does. But no society can change the nature of existence. We can’t prevent suffering. This pain and that pain, yes, but not Pain. A society can only relieve social suffering, unnecessary suffering. The rest remains. The root, the reality. All of us here are going to know grief; if we live fifty years, we’ll have known pain for fifty years… And yet, I wonder if it isn’t all a misunderstanding — this grasping after happiness, this fear of pain… If instead of fearing it and running from it, one could… get through it, go beyond it. There is something beyond it. It’s the self that suffers, and there’s a place where the self—ceases. I don’t know how to say it. But I believe that the reality — the truth that I recognize in suffering as I don’t in comfort and happiness — that the reality of pain is not pain. If you can get through it. If you can endure it all the way.'
+
 try:
     epd = epd7in5.EPD()
     epd.init()
@@ -12,9 +14,11 @@ try:
     print("Drawing")
     print("read bmp file on window")
     Himage2 = Image.new('1', (epd7in5.EPD_HEIGHT, epd7in5.EPD_WIDTH), 255)  # 255: clear the frame
-    bmp = Image.open('/home/pi/14.bmp')
-    Himage2.paste(bmp, (50,10))
-    epd.display(epd.getbuffer(Himage2))
+    # bmp = Image.open('/home/pi/14.bmp')
+    # Himage2.paste(bmp, (50,10))
+    # epd.display(epd.getbuffer(Himage2))
+    font24 = ImageFont.truetype('/usr/share/fonts/truetype/lato/Lato-Regular.ttf', 24)
+    draw.text((10, 0), ursula, font = font24, fill = 0)
 
     epd.sleep()
 
