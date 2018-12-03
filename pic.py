e9478615e7e3902a3a3a63a830b30efa0c062dee
sasham43 <sashasemail@gmail.com>
2018-12-02 18:02:14 -0600

    add pic displayer

---
+++ pic.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import epd as epd7in5
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+pic = '/home/pi/cover.bmp'
+
+try:
+    epd = epd7in5.EPD()
+    epd.init()
+    print("Clear")
+    epd.Clear(0xFF)
+
+    print("Drawing")
+    # print("read bmp file on window")
+    Himage2 = Image.new('1', (epd7in5.EPD_WIDTH, epd7in5.EPD_HEIGHT), 255)  # 255: clear the frame
+    bmp = Image.open(pic)
+    Himage2.paste(bmp, (50,10))
+
+    # write to display
+    epd.display(epd.getbuffer(Himage2))
+
+    print('sleeping')
+    epd.sleep()
+
+except:
+    print('traceback.format_exc():\n%s', traceback.format_exc())
+    exit()
