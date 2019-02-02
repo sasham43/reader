d7a74ee87745c0d5b778d90d05692cc7ea67e4fe
sasham43 <sashasemail@gmail.com>
2019-02-02 16:46:47 -0600

    try to iterate

--- vs.py
+++ vs.py
@@ -70,7 +70,8 @@ try:
     # print("Drawing")
 
     # go page by page
-    for index, page in enumerate(pages):
+    for index in enumerate(pages):
+        page = pages[index]
         print('drawing')
         image_time = time.time()
         Himage = Image.new('1', (epd7in5.EPD_HEIGHT, epd7in5.EPD_WIDTH), 255)  # 255: clear the frame
