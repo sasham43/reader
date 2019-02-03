16b73c26aeb5544f4bdbfd28bc364e2cc8c4d775
sasham43 <sashasemail@gmail.com>
2019-02-03 11:13:29 -0600

    fill black if not specified

--- pic.py
+++ pic.py
@@ -9,6 +9,10 @@ print "This is the name of the script: ", sys.argv[0]
 
 if len(sys.argv) > 1:
     pic = sys.argv[1]
+    if len(sys.argv) > 2 and sys.argv[2] == 'white':
+        fill = 255
+    else:
+        fill = 0
 else:
     pic = '/home/pi/cover.bmp'
 
@@ -20,7 +24,7 @@ try:
 
     print("Drawing")
     # print("read bmp file on window")
-    Himage2 = Image.new('1', (epd7in5.EPD_HEIGHT, epd7in5.EPD_WIDTH), 255)  # 255: clear the frame
+    Himage2 = Image.new('1', (epd7in5.EPD_HEIGHT, epd7in5.EPD_WIDTH), fill)  # 255: clear the frame
     bmp = Image.open(pic)
     # Himage2.paste(bmp, (50,10))
     Himage2.paste(bmp)
