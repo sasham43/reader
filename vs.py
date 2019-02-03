f90e384f6e9872722afda1427f6b0d27f2e8890a
sasham43 <sashasemail@gmail.com>
2019-02-03 16:52:00 -0600

    oh that's how that works

--- vs.py
+++ vs.py
@@ -52,72 +52,72 @@ def split_into_rows(input):
 
     return pages
 
-pages = split_into_rows(ursula)
-
-try:
-    init_time = time.time()
-    epd = epd7in5.EPD()
-    epd.init()
-    print('initialized', time.time() - init_time)
-
-    # fonts
-    font_base = '/usr/share/fonts/treutype/msttcorefonts/{file}'
-    font_file = 'Courier_New_Bold.ttf'
-    font24 = ImageFont.truetype(font_base.format(file=font_file), 24)
-
-    clear_time = time.time()
-    print('clearing')
-    epd.Clear(0xFF) # start off fresh
-    print('cleared', time.time() - clear_time)
-    # print("Drawing")
-
-    # go page by page
-    for index, page in enumerate(pages):
-        print(index, page)
-        # page = pages[page_number]
-        print('drawing')
-        image_time = time.time()
-        Himage = Image.new('1', (epd7in5.EPD_HEIGHT, epd7in5.EPD_WIDTH), 255)  # 255: clear the frame
-        draw = ImageDraw.Draw(Himage)
-        print('done drawing', time.time() - image_time)
-
-        # print(page)
-        for line in page:
-            draw.text((10, line_height), line, font = font24, fill = 0)
-            line_height = line_height_base + line_height
-            # print(line_height)
-        # save image
-
-        # Himage.save('/home/pi/page_{index}.png'.format(index=index), "PNG")
-        # reset line height
-        line_height = 0
-
-        # buffer_time = time.time()
-        # buffer = epd.getbuffer(Himage)
-        # print('buffered', time.time() - buffer_time)
-
-        # write to display
-        write_time = time.time()
-        print('writing')
-        epd.display(epd.getbuffer(Himage))
-        print('wrote', time.time() - write_time)
-
-        del draw
-        del Himage
-        # del buffer
-
-        # print('sleeping')
-        # epd.sleep()
-
-        print('new page')
-        # time.sleep(60)
-        # epd.Clear(0xFF) # start off fresh
-
-    # finished printing pages
-    time.sleep(5)
-    print('sleeping')
-    epd.sleep()
-
-except:
-    print('traceback.format_exc():\n%s', traceback.format_exc())
-    exit()
+# pages = split_into_rows(ursula)
+#
+# try:
+#     init_time = time.time()
+#     epd = epd7in5.EPD()
+#     epd.init()
+#     print('initialized', time.time() - init_time)
+#
+#     # fonts
+#     font_base = '/usr/share/fonts/treutype/msttcorefonts/{file}'
+#     font_file = 'Courier_New_Bold.ttf'
+#     font24 = ImageFont.truetype(font_base.format(file=font_file), 24)
+#
+#     clear_time = time.time()
+#     print('clearing')
+#     epd.Clear(0xFF) # start off fresh
+#     print('cleared', time.time() - clear_time)
+#     # print("Drawing")
+#
+#     # go page by page
+#     for index, page in enumerate(pages):
+#         print(index, page)
+#         # page = pages[page_number]
+#         print('drawing')
+#         image_time = time.time()
+#         Himage = Image.new('1', (epd7in5.EPD_HEIGHT, epd7in5.EPD_WIDTH), 255)  # 255: clear the frame
+#         draw = ImageDraw.Draw(Himage)
+#         print('done drawing', time.time() - image_time)
+#
+#         # print(page)
+#         for line in page:
+#             draw.text((10, line_height), line, font = font24, fill = 0)
+#             line_height = line_height_base + line_height
+#             # print(line_height)
+#         # save image
+#
+#         # Himage.save('/home/pi/page_{index}.png'.format(index=index), "PNG")
+#         # reset line height
+#         line_height = 0
+#
+#         # buffer_time = time.time()
+#         # buffer = epd.getbuffer(Himage)
+#         # print('buffered', time.time() - buffer_time)
+#
+#         # write to display
+#         write_time = time.time()
+#         print('writing')
+#         epd.display(epd.getbuffer(Himage))
+#         print('wrote', time.time() - write_time)
+#
+#         del draw
+#         del Himage
+#         # del buffer
+#
+#         # print('sleeping')
+#         # epd.sleep()
+#
+#         print('new page')
+#         # time.sleep(60)
+#         # epd.Clear(0xFF) # start off fresh
+#
+#     # finished printing pages
+#     time.sleep(5)
+#     print('sleeping')
+#     epd.sleep()
+#
+# except:
+#     print('traceback.format_exc():\n%s', traceback.format_exc())
+#     exit()
